17a9abdf3444096bd372bc75bceba6e3b4060cfc
kot_mech <babecaleksei@yandex.ru>
2023-10-16 18:23:34 +0700

    added tree view

--- src/components/TreeView.js
+++ src/components/TreeView.js
@@ -1,9 +1,42 @@
 import React, {useState} from "react";
+import {TreeItem} from "@mui/x-tree-view";
 
 function TreeView (content) {
-  const [treeContent, setTreeContent] = useState(content);
+  const [treeContent, setTreeContent] = useState(content.content);
+  const [categories, setCategories] = useState([]);
+  const [sorted, setSorted] = useState(false);
 
-  return (<div>Some tree view</div>)
+  if (!sorted) {
+    let sortedItems = [...treeContent].sort((a, b) => a.category > b.category ? 1 : -1);
+
+    let categories = [];
+    sortedItems.map((item) => {
+      if (!categories.includes(item.category)) categories.push(item.category);
+    })
+
+    setCategories(categories);
+    setTreeContent(sortedItems);
+    setSorted(true);
+  }
+
+  const getTreeLeafs = (category) => {
+    let items = treeContent.filter(item => item.category === category);
+    let leafs = [];
+
+    items.map(item => {leafs.push(<TreeItem nodeId={item.id} label={item.image.slice(item.image.indexOf('/')+1)}/>)})
+
+    console.log(items, leafs)
+    return leafs;
+  }
+
+  getTreeLeafs('animals')
+
+  return (
+    <div class={'tree-view'}>
+      <div>Some tree view</div>
+      <div>{getTreeLeafs('animals')}</div>
+    </div>
+  )
 }
 
 export default TreeView;
